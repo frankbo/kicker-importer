3eb16728c840acbe6400e8d738fca07c50ed935b
frankbo <frank.boeddeker@gmx.net>
2020-08-03 00:16:54 +0200

    Read file and test if it works

--- src/main/scala/kicker/importer/Main.scala
+++ src/main/scala/kicker/importer/Main.scala
@@ -1,9 +1,21 @@
 package kicker.importer
 
+import cats.effect._
 import cats.effect.{ExitCode, IO, IOApp}
+import scala.io.{Source, Codec}
 
 object Main extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] = IO("hello").map(_ => ExitCode.Success)
+  def readSourceFile(path: String): Resource[IO, Source] = {
+    val acquire = IO(Source.fromFile(path)(Codec.UTF8))
 
+    Resource.fromAutoCloseable(acquire)
+  }
 
+  override def run(args: List[String]): IO[ExitCode] = {
+    val resourcePath: String = getClass.getResource("/source.json").getPath
+    readSourceFile(resourcePath)
+      .use(source => IO(println(source.mkString))) //use ~ map on resource
+      .unsafeRunSync()
+    IO("hello").map(_ => ExitCode.Success)
+  }
 }
