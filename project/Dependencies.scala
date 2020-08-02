67751d8b19932c9ca9215ceb9e0dcf4b515749a2
frankbo <frank.boeddeker@gmx.net>
2020-08-02 23:21:41 +0200

    add plugins and correct dependency versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,24 +1,24 @@
 import sbt._
 
 object Dependencies {
-  private val circeVersion = "0.13.0"
-  private val Http4sVersion = "0.26.0"
+  val circeVersion = "0.13.0"
+  val Http4sVersion = "0.21.1"
 
-  private lazy val http4s = Seq(
+  lazy val http4s = Seq(
     "org.http4s" %% "http4s-blaze-client" % Http4sVersion,
     "org.http4s" %% "http4s-dsl" % Http4sVersion,
     "org.http4s" %% "http4s-circe" % Http4sVersion
   )
 
-  private lazy val circe = Seq(
+  lazy val circe = Seq(
     "io.circe" %% "circe-core",
     "io.circe" %% "circe-generic",
     "io.circe" %% "circe-parser"
   ).map(_ % circeVersion)
 
-  private lazy val scalaTest = Seq(
+  lazy val scalaTest = Seq(
     "org.scalatest" %% "scalatest" % "3.1.1" % Test
   )
 
-  lazy val dependencies: Seq[ModuleID] = scalaTest ++ circe ++ http4s
+  lazy val dependencies: Seq[ModuleID] = scalaTest ++ http4s ++ circe
 }
