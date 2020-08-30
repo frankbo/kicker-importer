a5277958f45a16cf1214298e1fd5ec4699c701c8
frankbo <frank.boeddeker@gmx.net>
2020-08-30 19:14:20 +0200

    Add circe-fs2 dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,8 @@ object Dependencies {
   lazy val circe = Seq(
     "io.circe" %% "circe-core",
     "io.circe" %% "circe-generic",
-    "io.circe" %% "circe-parser"
+    "io.circe" %% "circe-parser",
+    "io.circe" %% "circe-fs2"
   ).map(_ % circeVersion)
 
   lazy val scalaTest = Seq(
