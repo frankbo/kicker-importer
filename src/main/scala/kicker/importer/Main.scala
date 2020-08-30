df3fa90ccd4f31396755ffd27f7c3ed1d66b25b2
frankbo <frank.boeddeker@gmx.net>
2020-08-30 19:14:49 +0200

    Read file as stream and pipe it through http4s client

--- src/main/scala/kicker/importer/Main.scala
+++ src/main/scala/kicker/importer/Main.scala
@@ -1,21 +1,54 @@
 package kicker.importer
 
-import cats.effect._
-import cats.effect.{ExitCode, IO, IOApp}
-import scala.io.{Source, Codec}
+import java.nio.file.Paths
+
+import cats.effect.{ExitCode, IO, IOApp, _}
+import cats.implicits._
+import fs2.{Stream, text, io => fio}
+import io.circe.fs2.{decoder, stringArrayParser}
+import kicker.importer.Model.Location
+import org.http4s.client.Client
+import org.http4s.client.blaze._
+import org.http4s.implicits.http4sLiteralsSyntax
+import org.http4s.{Method, Request, Response, UrlForm}
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends IOApp {
-  def readSourceFile(path: String): Resource[IO, Source] = {
-    val acquire = IO(Source.fromFile(path)(Codec.UTF8))
+  def streamSourceFile(path: String, blocker: Blocker): Stream[IO, Location] = {
+    fio.file
+      .readAll[IO](
+        Paths.get(path),
+        blocker,
+        512
+      )
+      .through(text.utf8Decode)
+      .through(stringArrayParser)
+      .through(decoder[IO, Location])
+  }
 
-    Resource.fromAutoCloseable(acquire)
+  def importDatabase(
+      location: Location,
+      client: Client[IO]
+  ): Stream[IO, Response[IO]] = {
+    val request = Request[IO](
+      method = Method.POST,
+      uri = uri"https://hookb.in/RZjJOdjxQVFREEj72Lpn"
+    ).withEntity(
+      UrlForm(
+        "name" -> location.name
+      )
+    )
+    client.stream(request)
   }
 
   override def run(args: List[String]): IO[ExitCode] = {
-    val resourcePath: String = getClass.getResource("/source.json").getPath
-    readSourceFile(resourcePath)
-      .use(source => IO(println(source.mkString))) //use ~ map on resource
-      .unsafeRunSync()
-    IO("hello").map(_ => ExitCode.Success)
+    (for {
+      blocker <- Stream.resource(Blocker[IO])
+      client <- BlazeClientBuilder[IO](global).stream
+      location <-
+        streamSourceFile(getClass.getResource("/source.json").getPath, blocker)
+      response <- importDatabase(location, client)
+    } yield response).compile.drain.as(ExitCode.Success)
   }
 }
